15aa352666c902868b15093faa37dadb0c5a01de
alex <alexbonavila44@gmail.com>
2017-04-19 19:20:19 +0200

    Clock de w3schools

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/canvas', function (){
+    return view('canvas');
+});
+
 Route::group(['middleware' => 'auth'], function () {
     //    Route::get('/link1', function ()    {
 //        // Uses Auth Middleware
