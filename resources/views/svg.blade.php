2bee1a419fe196ec77e557c5eb43117bee1512c7
alex <alexbonavila44@gmail.com>
2017-04-19 19:50:08 +0200

    Exemple svg Javascript a pelo

--- resources/views/svg.blade.php
+++ resources/views/svg.blade.php
@@ -6,15 +6,32 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/9.7.3/css/bootstrap-slider.css" />
+
 </head>
 <body>
 <h1>SVG</h1>
 
+
+<input id="ex1" data-slider-id='ex1Slider' type="text" data-slider-min="50" data-slider-max="60" data-slider-step="1" data-slider-value="14"/>
+
 <svg width="300" height="300">
-    <circle cx="70" cy="70" r="60" stroke="green" stroke-width="4" fill="yellow" />
+    <circle id="circle" cx="70" cy="70" r="50" stroke="green" stroke-width="4" fill="yellow" />
 </svg>
 
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/9.7.3/bootstrap-slider.js"></script>
+
+<script>
+
+
+    // Without JQuery
+var slider = new Slider('#ex1', {
+	formatter: function(value) {
+        document.getElementById('circle').setAttribute('r', value );
+	}
+});
+</script>
 
 </body>
 </html>
