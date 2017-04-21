ca2fc82efeafa983ffa460b123491c52b207c6d4
alex <alexbonavila44@gmail.com>
2017-04-21 16:57:57 +0200

    Thinkering video.js primera versió

---
+++ resources/views/thinkeringVideoJS.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('htmlheader_title')
+	Change Title here!
+@endsection
+
+
+@section('main-content')
+	<div class="container-fluid spark-screen">
+		<div class="row">
+			<div class="col-md-9 col-md-offset-1">
+
+                <video src="http://clips.vorwaerts-gmbh.de/VfE_html5.mp4" id="demovideo1" class="video-js" controls data-setup="">
+                    <p class="vjs-no-js"> Sorry el teu navegado no suporta JS</p>
+                </video>
+
+            </div>
+		</div>
+	</div>
+@endsection
